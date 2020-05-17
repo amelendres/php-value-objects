4d286a1157232c7880ebbd35890916dd88c356d2
Al Fred Do <alfredo.melendres@gmail.com>
2020-05-17 10:27:33 +0200

    fix mother and added UuidTest with UuidMother

---
+++ tests/Unit/Appto/Common/Domain/Identity/UuidTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Test\Unit\Appto\Common\Domain\Identity;
+
+use Appto\Common\Domain\Identity\Uuid;
+use Appto\Common\Infrastructure\PHPUnit\Mother\UuidMother;
+use Appto\Common\Infrastructure\PHPUnit\UnitTest;
+
+class UuidTest extends UnitTest
+{
+    public function testCreateUuidSuccess() : void
+    {
+        $uuid = UuidMother::random(Uuid::class);
+        self::assertNotNull($uuid);
+    }
+}
