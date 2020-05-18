0aee3b441d6c3adb88f6e47c4686a250afa1bb32
Al Fred Do <alfredo.melendres@gmail.com>
2020-05-18 19:49:16 +0200

    fix CollectionMother

--- src/Appto/Common/Infrastructure/PHPUnit/Mother/CollectionMother.php
+++ src/Appto/Common/Infrastructure/PHPUnit/Mother/CollectionMother.php
@@ -6,20 +6,20 @@ use Appto\Common\Infrastructure\PHPUnit\Mother;
 
 class CollectionMother extends Mother
 {
-    public static function elements(Callable $objectMother, int $count=3)
+    public static function elements(callable $objectMother, $args, int $count = 3)
     {
         $elements = [];
         $i = 0;
         while ($i < $count) {
-            $elements[] = call_user_func($objectMother);
+            $elements[] = call_user_func($objectMother, $args);
             $i++;
         }
 
         return $elements;
     }
 
-    public static function randomFor(string $collectionFQNS, Callable $objectMother, int $count=10)
+    public static function randomFor(string $collectionFQNS, callable $objectMother, int $count = 10)
     {
-        return new $collectionFQNS(self::elements($objectMother, $count));
+        return new $collectionFQNS(self::elements($objectMother, null, $count));
     }
 }
