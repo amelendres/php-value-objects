52ebfb18066e62177e3da24b96ca1b1e381f5e26
Isai Baca <isaionline@gmail.com>
2021-02-10 13:10:08 +0100

    remove declare strict types

--- src/Appto/Common/Domain/Exception/DomainException.php
+++ src/Appto/Common/Domain/Exception/DomainException.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Appto\Common\Domain\Exception;
 
 class DomainException extends \DomainException
--- src/Appto/Common/Domain/Exception/InvalidArgumentException.php
+++ src/Appto/Common/Domain/Exception/InvalidArgumentException.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Appto\Common\Domain\Exception;
 
 interface InvalidArgumentException
